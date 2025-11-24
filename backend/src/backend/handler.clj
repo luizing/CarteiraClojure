280d86eb9dbfbe81e9d842c3d462f7267353b532
Luiz <luizeduardo2099@gmail.com>
2025-11-24 11:00:53 -0300

    backend -> feat: mapeamento dos endpoints e descrição de suas finalidade

--- backend/src/backend/handler.clj
+++ backend/src/backend/handler.clj
@@ -1,10 +1,16 @@
 (ns backend.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [cheshire.core :as json]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET "/acao" [] "Faz a requisição para API externa e busca os dados da ação enviadas via parametros da url ou corpo da requisição ou link")
+  (POST "/compra/acao" [] "Faz o registro de uma nova ação na carteira do usuario - Checa os dados da ação naquele momento")
+  (POST "/vende/acao" [] "Faz o registro da venda de uma ação na carteira do usuario - Adiciona os fundos a carteira e remove a ação da lista de ações")
+  (GET "/extrato" [] "Retorna o registro de compras e vendas em determinado período (enviado na requisição? enviar todos os registros divididos em períodos? dos ultimos x meses?)") 
+  (GET "/saldo" [] "Obter saldo da carteira")
   (route/not-found "Not Found"))
 
 (def app
