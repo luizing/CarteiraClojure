e8dd6c93968e125fea4868c2e31e89034a483bd8
Luiz <luizeduardo2099@gmail.com>
2025-11-26 13:58:30 -0300

    atualização gitignore

--- backend/src/backend/handler.clj
+++ backend/src/backend/handler.clj
@@ -12,9 +12,11 @@
   (let [baseUrl "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol="
         symbol (clojure.string/upper-case symbol)
         linkConection "&apikey="
-        requestLink (str baseUrl symbol linkConection APIKEY)]
+        sufixo ".SAO"
+        requestLink (str baseUrl symbol linkConection APIKEY sufixo) 
+        ]
     requestLink
-  ))
+    ))
 
 
 ;; No momento retorna o json puro, pode ser feito algum tratamento ainda no back, ou, posso deixar para o front.
@@ -29,8 +31,8 @@
   ;"Faz a requisição para API externa e busca os dados da ação enviadas via parametros da url ou corpo da requisição ou link"
   (GET "/acao/:symbol" [symbol] (consultaAcao symbol))
   (POST "/compra/acao" [] "Faz o registro de uma nova ação na carteira do usuario - Checa os dados da ação naquele momento")
-  (POST "/vende/acao" [] "Faz o registro da venda de uma ação na carteira do usuario - Adiciona os fundos a carteira e remove a ação da lista de ações")
-  (GET "/extrato" [] "Retorna o registro de compras e vendas em determinado período (enviado na requisição? enviar todos os registros divididos em períodos? dos ultimos x meses?)")
+  (POST "/vende/acao" [] "Faz o registro da venda de uma ação na carteira do usuario - remove a ação da lista de ações")
+  (GET "/extrato" [] "Retorna o registro de compras e vendas em determinado período (enviado na requisição)")
   (GET "/saldo" [] {:headers {"Content-Type" "application/json; charset=utf-8"}
                     :body (json/generate-string "Obter saldo da carteira")})
   (route/not-found "Not Found"))
